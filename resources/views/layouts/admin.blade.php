4643a9867c1799dcae15f3a36ff8c42dac8262d9
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-23 11:52:14 +0100

    Fixed technologies api

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ManuelFolio - BackOffice</title>
+    <title>ManuelFolio - Back Office</title>
 
 
     <!-- Fonts -->
