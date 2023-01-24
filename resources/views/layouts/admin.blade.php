e7f1d4f431ae3ffeaf0d7e20b95e4c25390ebb1a
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-24 18:20:33 +0100

    Fixed admin style

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,49 @@
 
 <body>
     <div id="app">
+        <!-- Header -->
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                <div class="container">
+                    <a class="navbar-brand d-flex align-items-center justify-content-between" href="{{ url('backoffice.manuelgaleone.it') }}">
+                        <div class="site_branding">
+                            <img width="50px" class="site_branding" src="https://i.ibb.co/kxjvhBj/identity-Tavola-disegno-1-copia-3.png" alt="Site Branding">
+                        </div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item text-center">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @else
+                            <li class="nav-item dropdown text-center">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('admin') }}">{{__('Pannello di Controllo')}}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- End of Header -->
         <main>
             <div id="wrapper">
                 <!-- Sidebar -->
@@ -42,24 +85,15 @@
                     </div>
                 </ul>
                 <!-- End of Sidebar -->
-
                 <!-- Content Wrapper -->
                 <div id="content-wrapper" class="d-flex flex-column">
                     <!-- Main Content -->
                     <div id="content">
-                        <!-- Topbar -->
+                        {{-- <!-- Topbar -->
                         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                             <div class="container">
-                                <!-- Left Side Of Navbar -->
-                                <div class="col-6 d-flex justify-content-start">
-                                    <ul class="navbar-nav me-auto">
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
-                                        </li>
-                                    </ul>
-                                </div>
                                 <!-- Right Side Of Navbar -->
-                                <div class="col-6 d-flex justify-content-end">
+                                <div class="col-6 d-flex justify-content-center">
                                     <ul class="navbar-nav ml-auto">
                                         <!-- Authentication Links -->
                                         @guest
@@ -94,8 +128,7 @@
                                 </div>
                             </div>
                         </nav>
-                        <!-- End of Topbar -->
-
+                        <!-- End of Topbar --> --}}
                         <!-- Begin Page Content -->
                         <div class="container-fluid">
                             @yield('dashboard')
@@ -104,24 +137,21 @@
                             @yield('edit')
                         </div>
                         <!-- /.container-fluid -->
-
                     </div>
                     <!-- End of Main Content -->
-
-                    <!-- Footer -->
-                    <footer class="sticky-footer bg-white">
-                        <div class="container my-auto">
-                            <div class="copyright text-center my-auto">
-                                <span>Copyright &copy; Manuel</span>
-                            </div>
-                        </div>
-                    </footer>
-                    <!-- End of Footer -->
-
                 </div>
                 <!-- End of Content Wrapper -->
             </div>
         </main>
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Manuel</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
     </div>
 </body>
 
